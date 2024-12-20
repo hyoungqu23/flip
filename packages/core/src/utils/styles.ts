eda40e538fb6ada422a18b5babb337dda862fbaa
HyoungMin <hyoungqu23@gmail.com>
2024-12-21 00:15:17 +0900

    기본으로 설정된 스타일 옵션과 커스터마이징 옵션을 병합하여 CSS 변수로 적용하는 `applyStyles` 유틸리티 함수를 추가한다

--- packages/core/src/utils/styles.ts
+++ packages/core/src/utils/styles.ts
@@ -1,3 +1,14 @@
+import type { TColor, TDimension, TDuration, TOptions } from '../types.ts';
+
+const defaultStyles = {
+  style: {
+    card: { width: 300, height: 400, borderRadius: 10, fontSize: 200 },
+    number: { color: '#FFFFFF', backgroundColor: '#000000' },
+    centerLine: { height: 10, backgroundColor: '#DDDDDD' },
+  },
+  animation: { duration: 500 },
+};
+
 export const setCSSVariables = (variables: Record<string, TDimension | TColor | TDuration>) => {
   Object.entries(variables).forEach(([key, value]) => {
     let formattedValue: string;
@@ -17,5 +28,23 @@ export const setCSSVariables = (variables: Record<string, TDimension | TColor | 
     }
 
     document.documentElement.style.setProperty(key, formattedValue);
+  });
+};
+
+export const applyStyles = (options?: TOptions) => {
+  setCSSVariables({
+    '--card-width': options?.style?.card?.width ?? defaultStyles.style.card.width,
+    '--card-height': options?.style?.card?.height ?? defaultStyles.style.card.height,
+    '--card-border-radius':
+      options?.style?.card?.borderRadius ?? defaultStyles.style.card.borderRadius,
+    '--card-font-size': options?.style?.card?.fontSize ?? defaultStyles.style.card.fontSize,
+    '--number-color': options?.style?.number?.color ?? defaultStyles.style.number.color,
+    '--number-bg-color':
+      options?.style?.number?.backgroundColor ?? defaultStyles.style.number.backgroundColor,
+    '--center-line-height':
+      options?.style?.centerLine?.height ?? defaultStyles.style.centerLine.height,
+    '--center-line-bg-color':
+      options?.style?.centerLine?.backgroundColor ?? defaultStyles.style.centerLine.backgroundColor,
+    '--animation-duration': options?.animation?.duration ?? defaultStyles.animation.duration,
   });
 };
