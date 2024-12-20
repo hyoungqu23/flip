fde8685be182853c5ee99aee303a84a95e160481
HyoungMin <hyoungqu23@gmail.com>
2024-12-21 00:21:39 +0900

    숫자 카드를 생성하는 `createCardElements` 함수를 추가한다

--- packages/core/src/dom/elements.ts
+++ packages/core/src/dom/elements.ts
@@ -1,4 +1,60 @@
+import type { TOptions } from '../types.ts';
+import { getDigits } from '../utils/digits';
 import { applyStyles, styleElementContent } from '../utils/styles';
+
+export const createCardElements = (
+  container: HTMLElement,
+  value: number,
+  isFirst: boolean,
+  options?: TOptions,
+) => {
+  if (!container || !(container instanceof HTMLElement)) {
+    throw new Error('Invalid Container Element.');
+  }
+
+  const digits = getDigits(value);
+
+  for (const currentDigit of digits) {
+    const { digit } = currentDigit;
+
+    const ul = document.querySelector<HTMLUListElement>(`ul.flip[data-digit="${digit}"]`);
+
+    if (!ul) {
+      const newUl = document.createElement('ul');
+      newUl.classList.add('flip');
+      newUl.dataset.digit = String(digit);
+
+      const fragment = document.createDocumentFragment();
+      for (let i = 9; i >= 0; i--) {
+        const li = document.createElement('li');
+        li.classList.add('flip', 'card');
+        li.dataset.number = String(i);
+
+        li.innerHTML = `
+          <div class="flip upper">
+            <span class="flip number">${i}</span>
+          </div>
+          <div class="flip lower">
+            <span class="flip number">${i}</span>
+          </div>
+        `;
+        fragment.appendChild(li);
+      }
+      newUl.appendChild(fragment);
+
+      if (isFirst) {
+        container.append(newUl);
+      } else {
+        container.prepend(newUl);
+      }
+    }
+  }
+
+  if (isFirst) {
+    applyStyles(options);
+    createStyleElement();
+  }
+};
 
 const createStyleElement = () => {
   const style = document.createElement('style');
