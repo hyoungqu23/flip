f2da43844341e4ca589229b56551e2f5df18c429
HyoungMin <hyoungqu23@gmail.com>
2024-12-21 00:21:18 +0900

    음수 부호 요소를 생성하는 `createMinusElement` 함수를 추가한다

--- packages/core/src/dom/elements.ts
+++ packages/core/src/dom/elements.ts
@@ -8,3 +8,18 @@ const createStyleElement = () => {
   document.head.appendChild(style);
 };
 
+export const createMinusElement = (container: HTMLElement, value: number) => {
+  const minus = document.querySelector('div.minus');
+
+  if (value < 0) {
+    if (!minus) {
+      const minusElement = document.createElement('div');
+      minusElement.classList.add('minus');
+      minusElement.textContent = '-';
+      container.prepend(minusElement);
+    }
+  } else {
+    minus?.remove();
+  }
+};
+
