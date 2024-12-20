03b16bfce1761e8b8ab58d049d8b30eb7e14e74c
HyoungMin <hyoungqu23@gmail.com>
2024-12-21 00:22:26 +0900

    현재 값과 다음 값을 기반으로 `class`를 추가, 제거하는 방식으로 애니메이션을 트리거하는 `applyAnimation` 함수를 추가한다

---
+++ packages/core/src/dom/animation.ts
@@ -0,0 +1,36 @@
+import type { TDirection } from '../types.ts';
+import { getDigits } from '../utils/digits';
+import { getConsecutiveNumber } from '../utils/number';
+
+export const applyAnimation = (value: number, direction: TDirection) => {
+  const currentDigits = getDigits(value);
+
+  for (const currentDigit of currentDigits) {
+    const { digit, value } = currentDigit;
+
+    const current = value;
+    const next = getConsecutiveNumber(value, direction);
+
+    const ul = document.querySelector<HTMLUListElement>(`ul.flip[data-digit="${digit}"`);
+
+    if (!ul) {
+      throw new Error('Not Found Container');
+    }
+
+    const lis = ul.querySelectorAll<HTMLLIElement>('li.flip.card');
+
+    lis.forEach((card) => {
+      card.classList.remove('current', 'next');
+    });
+
+    const currentLi = ul.querySelector<HTMLLIElement>(`li.card[data-number="${current}"]`);
+    const nextLi = ul.querySelector<HTMLLIElement>(`li.card[data-number="${next}"]`);
+
+    if (!currentLi || !nextLi) {
+      throw new Error('Not Found Cards');
+    }
+
+    currentLi.classList.add('current');
+    nextLi.classList.add('next');
+  }
+};
